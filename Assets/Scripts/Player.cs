94c8da907e17f063fedd69eca5d7a0ad38559338
SkunkDev <44464289+SkunkDev@users.noreply.github.com>
2022-04-09 12:52:16 +0200

    player and enemy scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float playerSpeed = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
