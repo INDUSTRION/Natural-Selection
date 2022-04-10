4cde99df17acd1da2ced4ac5decf8ceabf9602e5
INDUSTRION <danaipeter6@gmail.com>
2022-04-10 04:49:41 +0200

    Menu Scene

---
+++ Assets/Peter 1st Push/Script/MicroScope2.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MicroScope2 : MonoBehaviour
+{
+     public GameObject StartPanel;
+
+    public bool Prezz = false;
+
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+
+    void Update()
+    { 
+        if(Prezz == true)
+        {
+            Press();
+        }
+
+    }
+
+
+    void OnTriggerEnter(Collider colides)
+    {
+
+
+        if (colides.gameObject.tag == "Player")
+        {
+            StartPanel.SetActive(true);
+            Prezz = true;
+           
+           
+        }
+
+
+
+    }
+
+    void OnTriggerExit(Collider colides)
+    {
+
+
+        if (colides.gameObject.tag == "Player")
+        {
+            StartPanel.SetActive(false);
+
+            Prezz = false;
+        }
+
+
+
+    }
+
+
+
+    void Press()
+    {
+        if (Input.GetKey(KeyCode.E))
+
+        {
+
+
+            SceneManager.LoadScene("Falling");
+
+
+            // Scene.SceneManger.......
+
+        }
+    }
+}
